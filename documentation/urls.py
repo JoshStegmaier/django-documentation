615d85fd33b6481b9c9f7afa0e448761fee522d6
Josh Stegmaier <jrs@joshstegmaier.com>
2016-11-15 01:01:23 -0500

    Further 1.10 compat

--- documentation/urls.py
+++ documentation/urls.py
@@ -2,9 +2,11 @@ from django.conf.urls import url
 from django.contrib import admin
 admin.autodiscover()
 
+from .views import documentation
+
 urlpatterns = [
     'documentation.views',
 
-    url(r'^$', 'documentation', 'index.html'),
-    url(r'^(?P<path>.*)$', 'documentation', name="path"),
+    url(r'^$', documentation, 'index.html'),
+    url(r'^(?P<path>.*)$', documentation, name="path"),
 ]
